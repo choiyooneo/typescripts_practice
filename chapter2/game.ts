6c40887af3addb51bcf20dc7eb18809bd53e3cbc
yooknow <yooknow85@naver.com>
2023-05-29 17:33:32 +0900

    .

--- chapter2/game.ts
+++ chapter2/game.ts
@@ -1,12 +1,39 @@
 /**
  * Let's make a game 🕹
  */
+
+type cord = "up" | "down" | "left" | "right";
+
+let position = {
+  x: 0,
+  y: 0,
+};
+
+function move(cord: cord) {
+  switch (cord) {
+    case "up":
+      position.y += 1;
+      break;
+    case "down":
+      position.y -= 1;
+      break;
+    case "left":
+      position.x -= 1;
+      break;
+    case "right":
+      position.x += 1;
+      break;
+    default:
+      throw new Error("not valid cord");
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
